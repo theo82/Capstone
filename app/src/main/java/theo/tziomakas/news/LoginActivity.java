5f155838b405c53b6227e50712c49aaba443d84c
theo82 <theoaristi53@gmail.com>
2018-06-01 17:08:27 +0300

    Fixing an issue with back button in Login and Main Activities.

--- app/src/main/java/theo/tziomakas/news/LoginActivity.java
+++ app/src/main/java/theo/tziomakas/news/LoginActivity.java
@@ -119,7 +119,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void sendToMain() {
         Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
-        //mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(mainIntent);
         finish();
     }
--- app/src/main/java/theo/tziomakas/news/MainActivity.java
+++ app/src/main/java/theo/tziomakas/news/MainActivity.java
@@ -86,14 +86,9 @@ public class MainActivity extends AppCompatActivity {
     public void sendToStart(){
         Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
         startActivity(startIntent);
+        finish();
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
+
 
 }
