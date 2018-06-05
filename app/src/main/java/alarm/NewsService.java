a94b90e36e099fe1b5e723fb82d6773f4698d686
theo82 <theoaristi53@gmail.com>
2018-06-05 09:25:18 +0300

    Coming back to alarms.

---
+++ app/src/main/java/alarm/NewsService.java
@@ -0,0 +1,49 @@
+package alarm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import theo.tziomakas.news.model.News;
+import theo.tziomakas.news.utils.NetworkUtils;
+
+public class NewsService extends IntentService {
+
+    private String newsUrl;
+    private String date;
+    private SharedPreferences prefs;
+    List<News> newsList;
+
+
+    public NewsService() {
+        super("NewsService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        newsList = new ArrayList<>();
+        //newsUrl = bundle.getString(newsUrl);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        newsUrl = prefs.getString("url", "");
+
+
+        newsList = NetworkUtils.fetchNewsData(newsUrl);
+
+        String jsonNewsList = new Gson().toJson(newsList);
+
+        PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                .edit().putString("newsListData", jsonNewsList)
+                .commit();
+
+        Log.v("NewsService", String.valueOf(newsList));
+
+    }
+}
---
+++ app/src/main/java/alarm/WidgetBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class WidgetBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //String action = intent.getAction();
+        //Intent serviceIntent = new Intent(context, NewsService.class);
+        //context.startService(serviceIntent);
+    }
+}
